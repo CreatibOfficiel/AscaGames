7a03af2dc9b9b0f84db74d2c801c5b1bb2951a6c
Dylan Jacquet <d.jacquet@apparence.io>
2022-11-21 08:26:55 +0100

    Initial Commit

---
+++ AscaGames/AscaGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AscaGamesApp.swift
+//  AscaGames
+//
+//  Created by Dylan Jacquet on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AscaGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
