27466154841497ad391fedd33c4eb11b25843e6d
Thibaud Chaussabel Blachier <thibaud@auguste.video>
2022-11-25 14:55:21 +0100

    New buttons on ranking page

--- AscaGames/Views/UsersView/Components/RankingUsers.swift
+++ AscaGames/Views/UsersView/Components/RankingUsers.swift
@@ -10,15 +10,40 @@ import SwiftUI
 struct RankingUsers: View {
     
     @State var data : Array<User>
+    @State var singleMatch : Bool = true
     
     var switchIsRanking: () -> Void
     
     var body: some View {
         ScrollView {
             VStack {
-                VStack {
+                HStack {
+                    Button(action: swapMatchMode) {
+                        Text("1 vs 1")
+                            .padding()
+                            .foregroundColor(singleMatch ? theme.customYellow : .white)
+                            .frame(width: 100)
+                            .background(singleMatch ? .white : theme.customYellow)
+                            .clipShape(Capsule())
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(.white, lineWidth: singleMatch ? 0 : 2)
+                            )
+                    }.padding([.top,.bottom,.leading], 10)
+        
+                    Spacer()
                     
-                }.frame(width: 300, height: 60)
+                    Button(action: swapMatchMode) {
+                        Text("2 vs 2")
+                            .padding()
+                            .foregroundColor(singleMatch ? .white : theme.customYellow)
+                            .frame(width: 100)
+                            .background(singleMatch ? theme.customYellow : .white)
+                            .clipShape(Capsule())
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(.white, lineWidth: singleMatch ? 2 : 0)
+                            )
+                    }.padding([.top,.bottom,.trailing], 10)
+                }.frame(width: 300, height: 80)
                 .background(theme.customYellow)
                 
                 ScrollView {
@@ -47,5 +72,9 @@ struct RankingUsers: View {
             }.frame(width: 300, height: 500)
             .border(theme.customYellow, width: 5)
         }
+    }
+    
+    func swapMatchMode() -> Void {
+        singleMatch = !singleMatch
     }
 }
